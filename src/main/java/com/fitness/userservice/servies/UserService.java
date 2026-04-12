50cff760ca8705e68f1f1d409913a8e685fc1ab2
adding files <abhishek.20ec106@jssaten.ac.in>
2026-04-12 23:42:58 +0530

    adding kafka

--- src/main/java/com/fitness/userservice/servies/UserService.java
+++ src/main/java/com/fitness/userservice/servies/UserService.java
@@ -5,10 +5,12 @@ import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.models.User;
 import com.fitness.userservice.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository repository;
@@ -50,6 +52,7 @@ public class UserService {
     }
 
     public Boolean existBuUserId(String userId) {
+        log.info("Calling User Service {}", userId) ;
         return repository.existsById(userId);
     }
 }
