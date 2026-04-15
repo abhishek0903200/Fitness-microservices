5aa0e5f48d44f49c63a5f4cdea9d2addffe7d6c0
adding files <abhishek.20ec106@jssaten.ac.in>
2026-04-15 15:38:38 +0530

    Initial commit - fitness microservices

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    WALKING,
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+
+}
