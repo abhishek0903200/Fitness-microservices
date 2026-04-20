21a8e4588150c6a9b6970eb943b3b806cb1d73f0
adding files <abhishek.20ec106@jssaten.ac.in>
2026-04-20 11:25:13 +0530

    adding frontend features

--- fitness-frontend/src/App.jsx
+++ fitness-frontend/src/App.jsx
@@ -1,14 +1,38 @@
 // import './App.css'
 import Button from "@mui/material/Button";
-import { BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router"
+import { useContext, useState, useEffect} from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router";
+import {setCredentials} from "./store/authSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
 
+  const{ token, tokenData, logIn, logOut, isAuthenticated } 
+      = useContext(AuthContext); 
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);  
+
+  useEffect(() => {
+    if(token){
+      dispatch(setCredentials({token, user: tokenData}));
+      setAuthReady(true);
+    }
+  },[token, tokenData, dispatch]);
+
   return (
     <Router>
-      <Button variant="contained">
+      {!token ? (
+        <Button variant="contained" onClick={() => logIn()}>
         LOGIN
       </Button>
+      ) : (
+        <div>
+          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+          <pre>{token}</pre>
+        </div>
+      )
+    } 
     </Router>
   )
 }
--- fitness-frontend/src/main.jsx
+++ fitness-frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { Provider } from 'react-redux'
 import { store } from './store/store'
-
 import App from './App'
 import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from "./authConfig";
 
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'))
